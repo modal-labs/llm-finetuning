a753cdb200eb0254e6f77ab103cd9dc63f19d254
Richard Gong <richard@modal.com>
2023-11-24 18:39:00 +0000

    pre-release

---
+++ README.md
@@ -0,0 +1,98 @@
+# Fine-tune any LLM in minutes (ft. LLaMA, CodeLlama, Mistral)
+
+### Tired of prompt engineering? You've come to the right place.
+
+This no-frills guide will take you from a dataset to using a fine-tuned LLM
+model for inference in the matter of minutes. We walk through all the recommended, start-of-the-art optimizations for fast results:
+
+* *Deepspeed ZeRO-3* to efficiently shard the base model and training state across multiple GPUs [more info](https://www.deepspeed.ai/2021/03/07/zero3-offload.html)
+* *Parameter-efficient fine-tuning* via LoRa adapters for faster convergence
+* *Gradient checkpointing* to reduce VRAM footprint, fit larger batches and get higher training throughput
+
+The heavy lifting is done by the [`axolotl` framework](https://github.com/OpenAccess-AI-Collective/axolotl).
+
+Using Modal for fine-tuning means you never have to worry about infrastructure headaches like building images and provisioning GPUs. If a training script runs on Modal, it's reproducible and scalable enough to ship to production right away.
+
+### Just one local dependency - a Modal account
+
+1. Create a [Modal](https://modal.com/) account.
+2. Install `modal` in your current Python virtual environment (`pip install modal`)
+3. Set up a Modal token in your environment (`python3 -m modal setup`)
+4. You need to have a [secret](https://modal.com/secrets) named `huggingface` in your workspace. Populate both `HUGGING_FACE_HUB_TOKEN` and `HUGGINGFACE_TOKEN` with the same key from HuggingFace (settings under API tokens).
+5. For some LLaMA models, you need to go to the [Hugging Face page](https://huggingface.co/meta-llama/Llama-2-7b-chat-hf) and agree to the terms and conditions for access (granted instantly).
+
+### Code overview
+
+All the logic lies in `train.py`. There are three business Modal functions that will run in the cloud:
+
+* `new` creates a new folder in the `/runs` volume with the training config and data for a new training job. Also ensures the base model is downloaded from Hugginface.
+* `train` takes an existing folder, and performs the training job based on the config and data in that folder.
+* `Inference.completion` can spawn a vLLM inference container for any pre-trained model or fine-tuned model in a training folder.
+
+The rest of the code are helpers for _calling_ these three functions. There are two main ways to train:
+
+* Use the GUI to familiarize with the system (recommended for new fine-tuners!)
+* Use CLI commands (recommended for power users)
+
+### Using the GUI (simple Gradio interface)
+
+Deploy the training backend to expose the business functions (`new`, `train` and `completion`), then run the Gradio GUI.
+
+```bash
+modal deploy train.py
+modal run gui.py
+```
+
+The `*.modal.tunnel` link from the latter will take you to the Gradio GUI. There will be a tabs to launch training runs, test out trained models and explore the files on the volume.
+
+The difference between deploying an app and running an ephemeral app is that deployed apps will remain ready on the cloud for any invocations, whereas ephemeral apps will shut down once your local command exits. This lets your training jobs (that run on the deployed app) continue without your laptop; but makes sure you are not wasting resources hosting a GUI when not at your laptop.
+
+### Training
+
+Test out a simple training job with:
+
+```bash
+modal run train.py
+```
+
+Training depends on two files: `axolotl-mount/config.yml` and `axolotl-mount/my_data.jsonl`. The folder is automatically [mounted](https://modal.com/docs/guide/local-data#mounting-directories) to a remote container. When you make local changes to either of these files, they will be reflected in the next training run.
+
+
+
+The default configuration fine-tunes CodeLlama Instruct 7B to understand Modal documentation.
+
+produce SQL queries (10k examples trained for 10 epochs in about 30 minutes). The base model nicknames used can be configured in `common.py` and are used to define which model is being trained.
+
+Next, run inference to compare the results before/after training:
+```bash
+modal run inference.py --base chat7 --run-id chat7-sql --prompt '[INST] <<SYS>>
+You are an advanced SQL assistant that uses this SQL table schema to generate a SQL query which answers the user question.
+CREATE TABLE table_name_66 (points INTEGER, against VARCHAR, played VARCHAR)
+<</SYS>>
+
+What is the sum of Points when the against is less than 24 and played is less than 20? [/INST]'
+```
+
+Add `--batch 10000` to scale up seamlessly to dozens of GPUs for effortless parallelism as we complete 10000 prompts.
+
+<img width="874" alt="Screenshot 2023-09-16 at 1 29 39 AM" src="https://github.com/modal-labs/llama-finetuning/assets/8001209/d35bb956-dca2-4cc4-bb42-1e1372650481">
+
+### Fine-tuning on other models
+
+You can train it using other models, including the 70B variants. In order for that to work, you need to lower the batch size.
+
+```bash
+modal run train.py --dataset sql_dataset.py --base chat70 --run-id chat70-sql --batch-size 4
+```
+
+### Bring your own dataset
+
+Follow the example set by `sql_dataset.py` or `local_dataset.py` to import your own dataset. Then use
+
+```bash
+modal run validate_dataset.py --dataset new_dataset.py --base chat7 
+```
+
+to that validate your new script produces the desired training and test sets.
+
+*Tip: ensure your training set is large enough to run a single step (i.e. contains at least `N_GPUS * batch_size` rows) to avoid `torch` errors.*
--- gui.py
+++ gui.py
@@ -2,85 +2,42 @@
 
 import modal
 
-from train import VOLUME_CONFIG
-import time
+from train import APP_NAME, VOLUME_CONFIG
 
 stub = modal.Stub("example-axolotl-gui")
 
 gradio_image = modal.Image.debian_slim().pip_install("gradio==4.5.0")
 
-vllm_image = (
-    modal.Image.from_registry("nvcr.io/nvidia/pytorch:22.12-py3")
-    .pip_install(
-        "torch==2.0.1+cu118", index_url="https://download.pytorch.org/whl/cu118"
-    )
-    # Pinned to 10/16/23
-    .pip_install(
-        "vllm @ git+https://github.com/vllm-project/vllm.git@651c614aa43e497a2e2aab473493ba295201ab20"
-    )
-)
-
-@stub.cls(gpu="A100", image=vllm_image, volumes=VOLUME_CONFIG, allow_concurrent_inputs=60,  container_idle_timeout=120)
-class Model:
-    def __init__(self, model_path: str) -> None:
-        print(model_path)
-
-        from vllm.engine.arg_utils import AsyncEngineArgs
-        from vllm.engine.async_llm_engine import AsyncLLMEngine
-
-        engine_args = AsyncEngineArgs(model=model_path, gpu_memory_utilization=0.95)
-        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
-
-    @modal.method()
-    async def completion(self, input: str):
-        if not input:
-            return
-
-        from vllm.sampling_params import SamplingParams
-        from vllm.utils import random_uuid
-
-        sampling_params = SamplingParams(
-            presence_penalty=0.8,
-            temperature=0.2,
-            top_p=0.95,
-            top_k=50,
-            max_tokens=1024,
-        )
-        request_id = random_uuid()
-        results_generator = self.engine.generate(input, sampling_params, request_id)
-
-        t0 = time.time()
-        index, tokens = 0, 0
-        async for request_output in results_generator:
-            if "\ufffd" == request_output.outputs[0].text[-1]:
-                continue
-            yield request_output.outputs[0].text[index:]
-            index = len(request_output.outputs[0].text)
-
-            # Token accounting
-            new_tokens = len(request_output.outputs[0].token_ids)
-            tokens = new_tokens
-
-        throughput = tokens / (time.time() - t0)
-        print(f"Request completed: {throughput:.4f} tokens/s")
-        print(request_output.outputs[0].text)
-
-
 @stub.function(image=gradio_image, volumes=VOLUME_CONFIG, timeout=3600)
-def gui():
+def gui(config_raw: str, data_raw: str):
     import gradio as gr
     import glob
 
-    def process_train_files(config_yml, my_data_jsonl):
-        # Processing logic for training files
-        return "Training files processed."
+    # Find the deployed business functions to call
+    try:
+        new = modal.Function.lookup(APP_NAME, "new")
+        Inference = modal.Cls.lookup(APP_NAME, "Inference")
+    except modal.exception.NotFoundError:
+        raise Exception("Must first deploy training backend with `modal deploy train.py`.")
+
+    def launch_training_job(config_yml, my_data_jsonl):
+        run_id, handle = new.remote(config_yml, my_data_jsonl)
+        result = (
+            f"Started run {run_id} -> in folder /runs/{run_id}.\n\n"
+            f"Follow training logs at https://modal.com/logs/call/{handle.object_id}\n"
+        )
+        print(result)
+        return result
 
     def process_inference(model: str, input_text: str):
         text = f"Model: {model}\n{input_text}"
         yield text + "... (model loading)"
-        for chunk in Model(model.split("@")[-1]).completion.remote_gen(input_text):
-            text += chunk
-            yield text
+        try:
+            for chunk in Inference(model.split("@")[-1]).completion.remote_gen(input_text):
+                text += chunk
+                yield text
+        except Exception as e:
+            return repr(e)
 
     def model_changed(model):
         return f"Model changed to: {model}"
@@ -88,42 +45,60 @@ def gui():
     def get_model_choices():
         choices = [
             *glob.glob("/runs/*/lora-out/merged", recursive=True),
-            *glob.glob("/pretrained/models--*/snapshots/*", recursive=True)
+            *glob.glob("/pretrained/models--*/snapshots/*", recursive=True),
         ]
         choices = [f"{choice.split('/')[2]}@{choice}" for choice in choices]
         return gr.Dropdown(label="Select Model", choices=choices)
 
     with gr.Blocks() as interface:
         with gr.Tab("Train"):
-            with gr.Column():
-                config_input = gr.Textbox(label="config.yml", lines=10, placeholder="Enter YAML content here")
-                data_input = gr.Textbox(label="my_data.jsonl", lines=10, placeholder="Enter JSON Lines content here")
-            with gr.Column():
-                train_button = gr.Button("Launch training job")
-                train_output = gr.Textbox(label="Training details")
-                train_button.click(process_train_files, inputs=[config_input, data_input], outputs=train_output)
+            with gr.Row():
+                with gr.Column():
+                    config_input = gr.Code(label="config.yml", lines=20, value=config_raw)
+                    data_input = gr.Code(label="my_data.jsonl", lines=20, value=data_raw)
+                with gr.Column():
+                    train_button = gr.Button("Launch training job")
+                    train_output = gr.Markdown(label="Training details")
+                    train_button.click(
+                        launch_training_job,
+                        inputs=[config_input, data_input],
+                        outputs=train_output,
+                    )
 
         with gr.Tab("Inference"):
             with gr.Row():
                 with gr.Column():
                     model_dropdown = get_model_choices()
-                    input_text = gr.Textbox(label="Input Text", lines=10, placeholder="Enter text here")
+                    input_text = gr.Textbox(
+                        label="Input Text (please include prompt manually)",
+                        lines=10,
+                        value="[INST] How do I deploy a Modal function? [/INST]",
+                    )
                     inference_button = gr.Button("Run Inference")
+                    refresh_button = gr.Button("Refresh")
                 with gr.Column():
-                    inference_output = gr.Textbox(label="Output", lines=50)
-                    inference_button.click(process_inference, inputs=[model_dropdown, input_text], outputs=inference_output)
-            refresh_button = gr.Button("Refresh")
+                    inference_output = gr.Textbox(label="Output", lines=20)
+                    inference_button.click(
+                        process_inference,
+                        inputs=[model_dropdown, input_text],
+                        outputs=inference_output,
+                    )
             refresh_button.click(get_model_choices, inputs=None, outputs=model_dropdown)
-            model_dropdown.change(model_changed, inputs=model_dropdown, outputs=inference_output)
+            model_dropdown.change(
+                model_changed, inputs=model_dropdown, outputs=inference_output
+            )
 
         with gr.Tab("Files"):
             gr.FileExplorer(root="/runs")
 
-
     with modal.forward(8000) as tunnel:
         print("GUI available at", tunnel.url)
-        interface.launch(quiet=True, show_api=False, server_name="0.0.0.0", server_port=8000)
+        interface.launch(
+            quiet=True, show_api=False, server_name="0.0.0.0", server_port=8000
+        )
+
 
 @stub.local_entrypoint()
 def main():
-    gui.remote()
+    with open("config.yml", "r") as config, open("my_data.jsonl", "r") as data:
+        gui.remote(config.read(), data.read())
