ca9c72e36af9f09f0ee0395ac0e60648e995aa86
Richard Gong <richard@modal.com>
2023-12-08 20:32:32 +0000

    Patch LoRA merging

--- src/common.py
+++ src/common.py
@@ -9,18 +9,20 @@ axolotl_image = (
         "git clone https://github.com/OpenAccess-AI-Collective/axolotl /root/axolotl",
         "cd /root/axolotl && git checkout a581e9f8f66e14c22ec914ee792dd4fe073e62f6",
     )
-    .pip_install("huggingface_hub==0.17.1", "hf-transfer==0.1.3")
+    .pip_install("huggingface_hub==0.19.4", "hf-transfer==0.1.4")
     .env(dict(HUGGINGFACE_HUB_CACHE="/pretrained", HF_HUB_ENABLE_HF_TRANSFER="1"))
 )
 
-vllm_image = (
-    Image.from_registry("nvcr.io/nvidia/pytorch:23.10-py3")
-    .pip_install("vllm==0.2.3")
+vllm_image = Image.from_registry("nvcr.io/nvidia/pytorch:23.10-py3").pip_install(
+    "vllm==0.2.3"
 )
 
 stub = Stub(APP_NAME, secrets=[Secret.from_name("huggingface")])
 
 # Volumes for pre-trained models and training runs.
-pretrained_volume = Volume.persisted("example-pretrained-vol")
-runs_volume = Volume.persisted("example-runs-vol")
-VOLUME_CONFIG: dict[str | os.PathLike, Volume] = {"/pretrained": pretrained_volume, "/runs": runs_volume}
+pretrained_volume = Volume.persisted("axo-pretrained-vol")
+runs_volume = Volume.persisted("axo-runs-vol")
+VOLUME_CONFIG: dict[str | os.PathLike, Volume] = {
+    "/pretrained": pretrained_volume,
+    "/runs": runs_volume,
+}
--- src/train.py
+++ src/train.py
@@ -10,32 +10,63 @@ from .common import (
     VOLUME_CONFIG,
 )
 
-N_GPUS = os.environ.get("N_GPUS", 2)
-GPU_MEM = os.environ.get("GPU_MEM", 80)
+N_GPUS = int(os.environ.get("N_GPUS", 2))
+GPU_MEM = int(os.environ.get("GPU_MEM", 80))
 GPU_CONFIG = modal.gpu.A100(count=N_GPUS, memory=GPU_MEM)
 
 
+def print_common_training_issues(config):
+    min_train_tokens = (
+        config["sequence_len"]
+        * config["gradient_accumulation_steps"]
+        * config["micro_batch_size"]
+        * N_GPUS
+    )
+    print(
+        f"Please ensure there are enough tokens to train a single epoch of {min_train_tokens} tokens (recommended to have 4x)."
+    )
+
+    min_eval_samples = config["micro_batch_size"] * N_GPUS
+    print(
+        f"Please ensure there are enough samples for evaluation ({min_eval_samples})."
+    )
+
+
 @stub.function(
-    image=axolotl_image, gpu=GPU_CONFIG, volumes=VOLUME_CONFIG, memory=1024 * 100, timeout=3600 * 24
+    image=axolotl_image,
+    gpu=GPU_CONFIG,
+    volumes=VOLUME_CONFIG,
+    _allow_background_volume_commits=True,
+    retries=10,
 )
 def train(run_folder: str):
     print(f"Starting training run in {run_folder}")
 
     TRAIN_CMD = "accelerate launch -m axolotl.cli.train ./config.yml"
-    MERGE_CMD = "accelerate launch -m axolotl.cli.merge_lora ./config.yml --load_in_8bit=False --load_in_4bit=False"
+    if exit_code := subprocess.call(TRAIN_CMD.split(), cwd=run_folder):
+        exit(exit_code)
 
-    subprocess.call(TRAIN_CMD.split(), cwd=run_folder)
-    subprocess.call(MERGE_CMD.split(), cwd=run_folder)
+    return merge.spawn(run_folder)
 
-    print("Committing merged weights to", run_folder)
+
+@stub.function(image=axolotl_image, volumes=VOLUME_CONFIG, timeout=3600 * 24)
+def merge(run_folder: str):
+    import glob
+
+    checkpoints = glob.glob(f"./lora-out/checkpoint-*", root_dir=run_folder)
+    MERGE_SRC = max(checkpoints, key=lambda path: int(path.split("-")[-1]))
+    print(f"Merge from latest checkpoint {MERGE_SRC} in {run_folder}")
+
+    MERGE_CMD = f"accelerate launch -m axolotl.cli.merge_lora ./config.yml --lora_model_dir='{MERGE_SRC}' --load_in_8bit=False --load_in_4bit=False"
+    if exit_code := subprocess.call(MERGE_CMD.split(), cwd=run_folder):
+        exit(exit_code)
+
     VOLUME_CONFIG["/runs"].commit()
 
 
-# TODO(gongy): rename to start
 @stub.function(image=axolotl_image, timeout=60 * 30, volumes=VOLUME_CONFIG)
 def launch(config_raw: str, data_raw: str):
     from huggingface_hub import snapshot_download
-    from transformers.utils import move_cache
     import yaml
 
     # Ensure the base model is downloaded
@@ -47,8 +78,8 @@ def launch(config_raw: str, data_raw: str):
         snapshot_download(model_name, local_files_only=True)
         print(f"Volume contains {model_name}.")
     except FileNotFoundError:
-        print(f"Downloading {model_name} (no progress bar) ...")
-        snapshot_download(model_name, local_dir="/pretrained")
+        print(f"Downloading {model_name} ...")
+        snapshot_download(model_name)
 
         print("Committing /pretrained directory (no progress bar) ...")
         VOLUME_CONFIG["/pretrained"].commit()
@@ -68,7 +99,12 @@ def launch(config_raw: str, data_raw: str):
     VOLUME_CONFIG["/runs"].commit()
 
     # Start training run.
-    return run_folder, train.spawn(run_folder)
+    train_handle = train.spawn(run_folder)
+    with open(f"{run_folder}/logs.txt", "w") as f:
+        f.write(f"Logs at https://modal.com/logs/call/{train_handle.object_id}\n")
+    VOLUME_CONFIG["/runs"].commit()
+
+    return run_folder, train_handle
 
 
 @stub.local_entrypoint()
@@ -79,4 +115,5 @@ def main(config: str = "config.yml", dataset: str = "my_data.jsonl"):
         _, train_handle = launch.remote(cfg.read(), data.read())
 
     # Wait for the training run to finish.
-    train_handle.get()
+    merge_handle = train_handle.get()
+    merge_handle.get()
