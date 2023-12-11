7baa0189b6daa81ae1569979987bdfba0839b2a2
Richard Gong <richard@modal.com>
2023-12-11 17:26:28 +0000

    pin axolotl docker image

--- src/common.py
+++ src/common.py
@@ -3,15 +3,19 @@ import os
 
 APP_NAME = "example-axolotl"
 
+# Latest image hash of winglian/axolotl:main-py3.10-cu118-2.0.1 (2023-12-11)
+AXOLOTL_REGISTRY_SHA = "5c19a5154fd522225953b9c3f6206750f4191e0e92ee424f02963f7963ada698"
+# Need to patch transformers to an older version to avoid checkpointing errors.
+TRANSFORMERS_SHA = "5324bf9c07c318015eccc5fba370a81368a8df28"
+
 axolotl_image = (
-    Image.from_registry("winglian/axolotl:main-py3.10-cu118-2.0.1")
+    Image.from_registry(f"winglian/axolotl@sha256:{AXOLOTL_REGISTRY_SHA}")
     .run_commands(
         "git clone https://github.com/OpenAccess-AI-Collective/axolotl /root/axolotl",
         "cd /root/axolotl && git checkout a581e9f8f66e14c22ec914ee792dd4fe073e62f6",
     )
     .pip_install("huggingface_hub==0.19.4", "hf-transfer==0.1.4")
-    # Atomic checkpoints in this transformers release
-    # .pip_install("transformers @ git+https://github.com/huggingface/transformers.git@4c5ed1d0c942dd4a60e1f99d6636519c40f7c904")
+    .pip_install(f"transformers @ git+https://github.com/huggingface/transformers.git@{TRANSFORMERS_SHA}", "--force-reinstall")
     .env(dict(HUGGINGFACE_HUB_CACHE="/pretrained", HF_HUB_ENABLE_HF_TRANSFER="1"))
 )
 
