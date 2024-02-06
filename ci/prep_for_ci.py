0b8735b6eaad8dacb22b211825a88f7f042d8b25
Michael Waskom <michael@modal.com>
2024-02-06 18:15:10 -0500

    Remove sample packing and standardize batch / LR params for all models

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -7,19 +7,18 @@ import yaml
 @click.option("--data")
 def main(config: str, data: str):
     """Set the config to train for only one epoch and truncate the dataset."""
+    train_set_size = 1000
+    val_set_size = 1000
     with open(config) as f:
         cfg = yaml.safe_load(f.read())
-    cfg["sample_packing"] = False
-    cfg["pad_to_sequence_len"] = False
-    cfg["micro_batch_size"] = 32
-    cfg["gradient_accumulation_steps"] = 1
-    cfg["learing_rate"] = 0.0001
+    cfg["val_set_size"] = val_set_size
     cfg["num_epochs"] = 1
+    cfg.pop("eval_steps", None)  # Evaluate once at the end of the epoch
     with open(config, "w") as f:
         yaml.dump(cfg, f)
 
     with open(data) as f:
-        data_truncated = f.readlines()[:1000]
+        data_truncated = f.readlines()[: train_set_size + val_set_size]
     with open(data, "w") as f:
         f.writelines(data_truncated)
 
