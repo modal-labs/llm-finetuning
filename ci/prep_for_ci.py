009849b0e9fe03bdefd18549d4ca88b40ed26ea7
Michael Waskom <michael@modal.com>
2024-02-06 13:36:13 -0500

    Don't watch gradients in mistral; set eval_steps to default

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -11,10 +11,10 @@ def main(config: str, data: str):
         cfg = yaml.safe_load(f.read())
     cfg["sequence_len"] = 1024
     cfg["val_set_size"] = 100
-    cfg["eval_steps"] = 1000
     cfg["eval_batch_size"] = 10
     cfg["micro_batch_size"] = 10
     cfg["num_epochs"] = 2
+    cfg.pop("eval_steps", None)
     with open(config, "w") as f:
         yaml.dump(cfg, f)
 
