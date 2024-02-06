f21f60edbf3d7eff5b1ec50dde0e9d7586ff1292
Michael Waskom <michael@modal.com>
2024-02-06 11:20:11 -0500

    Set eval steps and batch size for CI

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -11,6 +11,8 @@ def main(config: str, data: str):
         cfg = yaml.safe_load(f.read())
     cfg["sequence_len"] = 1024
     cfg["val_set_size"] = 100
+    cfg["eval_steps"] = 1
+    cfg["eval_batch_size"] = 10
     cfg["num_epochs"] = 2
     with open(config, "w") as f:
         yaml.dump(cfg, f)
