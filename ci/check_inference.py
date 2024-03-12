da5871d71410ab2c35b92ddc957f43d9098ad6c2
Erik Bernhardsson <mail@erikbern.com>
2024-03-12 17:36:23 -0400

    No need to pass --run-name

--- ci/check_inference.py
+++ ci/check_inference.py
@@ -2,15 +2,11 @@ import subprocess
 
 
 if __name__ == "__main__":
-
-    with open(".last_run_name", "r") as f:
-        run_name = f.read().strip()
-
     prompt = """[INST] Using the schema context below, generate a SQL query that answers the question.
 CREATE TABLE head (age INTEGER)
 How many heads of the departments are older than 56 ? [/INST] """
 
-    p = subprocess.Popen(["modal", "run", "src.inference", "--run-name", run_name, "--prompt", prompt], stdout=subprocess.PIPE)
+    p = subprocess.Popen(["modal", "run", "src.inference", "--prompt", prompt], stdout=subprocess.PIPE)
     output = ""
 
     for line in iter(p.stdout.readline, b''):
