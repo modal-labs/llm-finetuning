0e0673b8eed5c0ead29340c0e5e7f1a7fc182ee8
Charles Frye <charles@modal.com>
2024-05-23 23:17:44 +0000

    adds wandb to CI, fixes seed

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -10,12 +10,15 @@ def main(config: str, data: str):
     with open(config) as f:
         cfg = yaml.safe_load(f.read())
 
+    cfg["wandb_project"] = "ci-llm-finetuning-overfit-sqlqa"
+    cfg["seed"] = 117  # always set a seed
+
     num_epochs = 50
     val_set_size = 0.5
 
     cfg["val_set_size"] = val_set_size
     cfg["num_epochs"] = num_epochs
-    cfg["eval_steps"] = num_epochs
+    cfg["eval_steps"] = num_epochs // 10
     cfg.pop("evals_per_epoch", None)  # incompatible with eval_steps
     cfg.pop("sample_packing", False)  # requires larger dataset
 
