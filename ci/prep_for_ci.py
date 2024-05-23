1bf06680f4bb1a4376ded67431177d5a59c0773c
Charles Frye <charles@modal.com>
2024-05-23 20:56:13 +0000

    update ci tests

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -6,15 +6,12 @@ import yaml
 @click.option("--config")
 @click.option("--data")
 def main(config: str, data: str):
-    """Set the config to train for only one epoch and truncate the dataset."""
+    """Set the config to run for many epochs to test overfitting."""
     with open(config) as f:
         cfg = yaml.safe_load(f.read())
 
     num_epochs = 50
     val_set_size = 0.5
-
-    if cfg["base_model"] == "mistralai/Mixtral-8x7B-Instruct-v0.1":
-        num_epochs = 25  # mixtral training is slower and not well-tuned, cut early
 
     cfg["val_set_size"] = val_set_size
     cfg["num_epochs"] = num_epochs
