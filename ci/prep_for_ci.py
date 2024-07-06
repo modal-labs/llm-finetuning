1c2e6121f3ef07aacb8222f5b3e34674d420f934
Charles Frye <charles@modal.com>
2024-07-06 05:36:06 +0000

    give codellama back a few epochs

--- ci/prep_for_ci.py
+++ ci/prep_for_ci.py
@@ -20,7 +20,9 @@ def main(config: str, data: str):
     val_set_size = 0.5
 
     num_epochs = 25
-    if "pythia-1.4b" in cfg["base_model"]:
+    if "CodeLlama-7b" in cfg["base_model"]:
+        num_epochs = num_epochs + 10
+    elif "pythia-1.4b" in cfg["base_model"]:
         num_epochs = num_epochs * 3
 
     cfg["val_set_size"] = val_set_size
